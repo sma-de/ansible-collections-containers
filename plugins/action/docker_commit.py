2070dbc0268b0a7cd0c21116a21988dbe8b24430
Mirko Wilhelmi <Mirko.Wilhelmi@sma.de>
2020-10-07 11:19:17 +0200

    merge feature: added module / action plugin ...
    ... for properly handling docker commits
    
    Squashed commit of the following:
    
    commit f8a05779d88680418c9d3cd54c048bf3056ef136
    Author: Mirko Wilhelmi <Mirko.Wilhelmi@sma.de>
    Date:   Tue Oct 6 23:41:06 2020 +0200
    
        dcommit: handle checking for existing 07
    
    commit 7218f68fad8be6cd803d941dc75420a86d10a95c
    Author: Mirko Wilhelmi <Mirko.Wilhelmi@sma.de>
    Date:   Tue Oct 6 23:32:26 2020 +0200
    
        dcommit: handle checking for existing 06
    
    commit 76c4ee3d4f553d03fac2f1871469f872f576389f
    Author: Mirko Wilhelmi <Mirko.Wilhelmi@sma.de>
    Date:   Tue Oct 6 23:28:36 2020 +0200
    
        dcommit: handle checking for existing 05
    
    commit dbde7c6cecdfd7ac958579f9bbc71d5293416976
    Author: Mirko Wilhelmi <Mirko.Wilhelmi@sma.de>
    Date:   Tue Oct 6 23:09:27 2020 +0200
    
        dcommit: handle checking for existing 04
    
    commit 0400065b30fd747dbdaf06cd0a8fa6a5eb178109
    Author: Mirko Wilhelmi <Mirko.Wilhelmi@sma.de>
    Date:   Tue Oct 6 23:01:35 2020 +0200
    
        dcommit: handle checking for existing 03
    
    commit e5117b02f819ab5844665acb90c5d06b649652a2
    Author: Mirko Wilhelmi <Mirko.Wilhelmi@sma.de>
    Date:   Tue Oct 6 22:59:04 2020 +0200
    
        dcommit: handle checking for existing 02
    
    commit cd0b2834b4056b2e6178c362ea10c518add3b2d5
    Author: Mirko Wilhelmi <Mirko.Wilhelmi@sma.de>
    Date:   Tue Oct 6 22:42:43 2020 +0200
    
        dcommit: handle checking for existing
    
    commit 4f603fd73555403a2a4759615cdf8cfbe176f026
    Author: Mirko Wilhelmi <Mirko.Wilhelmi@sma.de>
    Date:   Tue Oct 6 22:17:53 2020 +0200
    
        support CMD/ENTRYPOINT as list input
    
    commit f69fcd8101b6a07c058752ba1ebc8c402fa00bea
    Author: Mirko Wilhelmi <Mirko.Wilhelmi@sma.de>
    Date:   Tue Oct 6 21:45:48 2020 +0200
    
        bugfixing docker committing 03
    
    commit 672b4e9bf11dc8fec488d65e02b336f5b21a069c
    Author: Mirko Wilhelmi <Mirko.Wilhelmi@sma.de>
    Date:   Tue Oct 6 21:43:12 2020 +0200
    
        bugfixing docker committing 02
    
    commit 0ce1b64fdb9b16f6a297f5e17adcc966e5588b3a
    Author: Mirko Wilhelmi <Mirko.Wilhelmi@sma.de>
    Date:   Tue Oct 6 21:22:15 2020 +0200
    
        added new module / action plugin for handling ...
    
        ... docker commits

---
+++ plugins/action/docker_commit.py
@@ -0,0 +1,136 @@
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+
+import collections
+import json
+import re
+
+##from ansible.errors import AnsibleOptionsError, AnsibleModuleError##, AnsibleError
+####from ansible.module_utils._text import to_native
+from ansible.module_utils.six import iteritems, string_types
+
+from ansible_collections.smabot.base.plugins.module_utils.plugins.action_base import BaseAction
+
+##from ansible_collections.smabot.base.plugins.module_utils.utils.dicting import get_subdict, SUBDICT_METAKEY_ANY
+##from ansible_collections.smabot.base.plugins.module_utils.utils.utils import ansible_assert
+##from ansible_collections.smabot.containers.plugins.module_utils.common import DOCKER_CFG_DEFAULTVAR
+
+
+class ActionModule(BaseAction):
+
+    def __init__(self, *args, **kwargs):
+        super(ActionModule, self).__init__(*args, **kwargs)
+        self._supports_check_mode = False
+        self._supports_async = False
+
+
+    @property
+    def argspec(self):
+        tmp = super(ActionModule, self).argspec
+
+        dkeys_subspec = {
+          'CMD': (list(string_types) + [list(string_types)], []),
+          'ENTRYPOINT': (list(string_types) + [list(string_types)], []),
+          'ENV': ([collections.abc.Mapping], {}),
+          'EXPOSE': ([list(string_types)], []),
+          'LABEL': ([list(string_types)], []),
+          'USER': (list(string_types), ''),
+          'WORKDIR': (list(string_types), ''),
+        }
+
+        tmp.update({
+          'container': (list(string_types)),
+          'image_name': (list(string_types)),
+          'image_tag': (list(string_types), ''),
+          'force': ([bool], False),
+
+          'docker_keywords': ([collections.abc.Mapping], {}, dkeys_subspec),
+        })
+
+        return tmp
+
+
+    def run_specific(self, result):
+        imgname = self.get_taskparam('image_name')
+        imgtag = self.get_taskparam('image_tag')
+        force = self.get_taskparam('force')
+
+        img_fullname = imgname
+
+        if imgtag:
+            img_fullname += ':' + imgtag
+
+        ## image exists means for us something with given name 
+        ## already exists on docker build node or is pullable
+        pullmod = 'community.general.docker_image'
+        mres = self.exec_module(pullmod, 
+          modargs=dict(name=img_fullname, source='pull'), ignore_error=True
+        )
+
+        ## image already exists on node => pull modules returns green
+        ## image is pullable => pull modules returns yellow
+        ## image does not exist yet => pull modules fails
+        img_exists = True
+
+        if mres.get('failed', False):
+            ## check if it failed simply because image does not 
+            ## exist yet or if something is actually really wrong
+
+            if not re.search(r'(?i)404 client error: not found', mres['msg']):
+                self._rescheck_inner_call(mres, pullmod, 'MODULE')
+
+            img_exists = False
+
+        if img_exists and not force:
+            ## image already exists and force is unset, noop
+            result['msg'] = \
+               "Image with given name '{}' already exists, will not"\
+               " change it unless force is set to true".format(img_fullname)
+
+            return result
+
+        ##
+        ## note: we only do basic name checking, no advanced testing 
+        ##   if existing image and container to commit are identical 
+        ##   or not based on content, so like updating a password or 
+        ##   doing a git commit, if we come here, this is an ever changer
+        ##
+        ## TODO: there is not really a feasible way to determine if the container to commit and a possible pre-existing image with the same name are idempotent or not, or is there ??
+        ##
+        result['changed'] = True
+
+        container = self.get_taskparam('container')
+
+        ## build commit command
+        commit_command = ['docker', 'commit']
+
+        ## handle docker keywords
+        json_keys = ['CMD', 'ENTRYPOINT']
+        for (k, v) in iteritems(self.get_taskparam('docker_keywords')):
+            if not v: continue
+
+            if isinstance(v, collections.abc.Mapping):
+                tmp = []
+
+                for (vk, vv) in iteritems(v):
+                    tmp.append("{}={}".format(vk, vv))
+
+                v = tmp
+            elif isinstance(v, list):
+                if k in json_keys:
+                    v = [json.dumps(v)]
+            else:
+                v = [v]
+
+            for x in v:
+                x = "{} {}".format(k, x)
+                commit_command += ['-c', x]
+
+        commit_command += [container, img_fullname]
+
+        mres = self.exec_module('command', modargs={'argv': commit_command})
+
+        return result
+
