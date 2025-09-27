5d8faa2471475218bf2994939dddf0ec4e258e3e
Mirko Wilhelmi <Mirko.Wilhelmi@sma.de>
2025-09-27 19:52:31 +0200

    wip

--- plugins/filter/cont_building.py
+++ plugins/filter/cont_building.py
@@ -49,6 +49,16 @@ class AutoVersionPostProc(FilterBase):
           'capture_group': ([collections.abc.Mapping], {}, cgrp_spec),
         })
 
+        mucap_spec = {
+          'regex': (list(string_types), ''),
+          'versions': ([collections.abc.Mapping], {}),
+        }
+
+        tmp.update({
+          'multi_capture': ([collections.abc.Mapping], {}, mucap_spec),
+        })
+
+        tmp['mutual_exclusions'] = [['capture_group', 'multi_capture']]
         return tmp
 
 
@@ -84,6 +94,81 @@ class AutoVersionPostProc(FilterBase):
                 raise AnsibleOptionsError(errpxf + " no match was found")
 
             value = found_match
+            return value
+
+        mcap = self.get_taskparam('multi_capture')
+        rgx = mcap['regex']
+        vers = mcap['versions']
+
+        if rgx:
+            res = {'extra_tags': [], 'meta_info': {}}
+
+            ## create final regex from regex template and defined versions
+            tmp = {}
+
+            if 'idtag' not in vers:
+                raise AnsibleOptionsError(
+                    "mandatory versions subkey 'idtag' missing:\n"\
+                    "  {}".format(
+                        json.dumps(mcap, indent=2).replace('\n', '\n  ')
+                    )
+                )
+
+            for k in list(vers.keys()):
+                v = vers[k]
+
+                if not isinstance(v, collections.abc.Mapping):
+                    ## assume simple pattern string
+                    v = {'pattern': v}
+
+                if k == 'idtag':
+                    if v.get('optional', False):
+                        raise AnsibleOptionsError(
+                            "special versions subkey 'idtag' cannot be"\
+                            " optional:\n  {}".format(
+                                json.dumps(mcap, indent=2).replace(
+                                    '\n', '\n  '
+                                )
+                            )
+                        )
+
+                tmp[k.upper()] = (r'(?P<' + k + '>{})').format(v['pattern'])
+                vers[k] = v
+
+            rgx_fin = rgx.format(**tmp)
+
+            errpfx = \
+               "multi capture must match exactly once, but for"\
+               " given regex template '{}' and final templated"\
+               " regex '{}'".format(rgx, rgx_fin)
+
+            found_matches = {}
+            for m in re.finditer(rgx_fin, value, flags=re.MULTILINE):
+                if found_matches:
+                    raise AnsibleOptionsError(errpxf + " more than one match was found")
+
+                for k, v in vers.items():
+                    mx = m.group(k)
+
+                    if mx:
+                        found_matches[k] = mx
+                        continue
+
+                    if not v.get('optional', False):
+                        raise AnsibleOptionsError(
+                            "failed to find a match for mandatory versions"\
+                            " subgroup '{}' using regex '{}' trying to"\
+                            " match on this value:\n  '{}'".format(
+                                k, rgx_fin, value
+                            )
+                        )
+
+            if not found_matches:
+                raise AnsibleOptionsError(errpxf + " no match was found")
+
+            res['idtag'] = found_matches['idtag']
+            res['ver_in'] = value
+            return res
 
         return value
 
